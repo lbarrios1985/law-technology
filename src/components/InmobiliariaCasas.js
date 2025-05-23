6cc6c17b5fda439db12a1c8b876c7a4e2627b534
Mayret Aguilar <amayaguilar548@gmail.com>
2025-05-23 13:30:47 -0400

    inmobiliaria

--- src/components/InmobiliariaCasas.js
+++ src/components/InmobiliariaCasas.js
@@ -1,20 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import { Typography, Container } from "@mui/material";
 
-const InmobiliariaCasas = () => (
-  <Container style={{ padding: "2rem 0" }}>
-    <Typography variant="h4" gutterBottom>
-      Casas
-    </Typography>
-    <Typography variant="body1">
-      Aquí encontrarás información sobre casas disponibles.
-    </Typography>
-  </Container>
-);
-
-export default InmobiliariaCasas;
-=======
 import {
   Box,
   Container,
@@ -264,4 +250,4 @@ const InmobiliariaCasas = () => {
 
 export default InmobiliariaCasas;
 
->>>>>>> de4a2cc (inmobiliaria)
+
